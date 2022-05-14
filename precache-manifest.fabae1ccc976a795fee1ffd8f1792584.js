ce075148a432c5f9d96540bb298132b13a8cc1d9
2019010503 <2019010503@secoder.net>
2022-05-14 20:45:01 +0800

    Updates

---
+++ precache-manifest.fabae1ccc976a795fee1ffd8f1792584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2aca62644d560889bbecc81f234631",
+    "url": "/blockchain-ticket-system/index.html"
+  },
+  {
+    "revision": "34ce63922b2749bafffb",
+    "url": "/blockchain-ticket-system/static/css/main.7bd11ebf.chunk.css"
+  },
+  {
+    "revision": "4c835e35d7793c742993",
+    "url": "/blockchain-ticket-system/static/js/2.7ad9d244.chunk.js"
+  },
+  {
+    "revision": "34ce63922b2749bafffb",
+    "url": "/blockchain-ticket-system/static/js/main.5772b5b9.chunk.js"
+  },
+  {
+    "revision": "93f868b2e89e0823f31a",
+    "url": "/blockchain-ticket-system/static/js/runtime-main.fe86a376.js"
+  }
+]);
