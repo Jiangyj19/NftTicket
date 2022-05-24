23c5f151230f88ec22b30b42ef624d7fb90f46ca
2019010503 <2019010503@secoder.net>
2022-05-24 16:27:08 +0800

    Updates

---
+++ precache-manifest.c66d10b4461806a90858957213076ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1d226863f31bdea3e947fd17e1f4c7",
+    "url": "/NftTicket/index.html"
+  },
+  {
+    "revision": "91cfb77ee0108c98969f",
+    "url": "/NftTicket/static/css/main.1a7a4728.chunk.css"
+  },
+  {
+    "revision": "76d5dfcc6e41db870351",
+    "url": "/NftTicket/static/js/2.71c41df1.chunk.js"
+  },
+  {
+    "revision": "91cfb77ee0108c98969f",
+    "url": "/NftTicket/static/js/main.75297df4.chunk.js"
+  },
+  {
+    "revision": "ba2e707b39c2a3f1cd36",
+    "url": "/NftTicket/static/js/runtime-main.6ec55b57.js"
+  }
+]);
